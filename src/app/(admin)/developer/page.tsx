c7c672615efbdc27ca3d5748b322bdebc3e661ca
zengxpang <zengxpang@163.com>
2023-07-18 09:41:54 +0800

    feat: second commit

--- src/app/(admin)/developer/page.tsx
+++ src/app/(admin)/developer/page.tsx
@@ -3,7 +3,11 @@ interface IDeveloper {
 
 const page = (props: IDeveloper) => {
     return (
-        <div>Developer</div>
+        <>
+            <div className='w-32 h-32 bg-orange-500 m-2 text-center'>text</div>
+            <div className="menu">首页</div>
+            <div className="w-32 h-32 bg-blue-500 dark:bg-green-500">暗黑模式</div>
+        </>
     );
 };
 
